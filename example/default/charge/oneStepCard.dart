b06140ffed44f1c549ca8b048fee682b7dedcfc8
Guilherme Cota <75252715+guilhermecotaGn@users.noreply.github.com>
2023-03-31 11:26:02 -0300

    Update oneStepCard.dart

--- example/default/charge/oneStepCard.dart
+++ example/default/charge/oneStepCard.dart
@@ -5,11 +5,20 @@ void main() async {
   credentials.remove('pix_cert');
   credentials.remove('pix_private_key');
   Gerencianet gn = Gerencianet(credentials);
-  dynamic response = await createChargeOneStep(gn);
+  Map<String, Object> card = {
+    "brand": "",
+    "number": "",
+    "cvv": "",
+    "expiration_month": "",
+    "expiration_year": ""
+  };
+  dynamic response = createCharge(gn, card);
   print(response);
 }
 
-dynamic createChargeOneStep(Gerencianet gn) async {
+dynamic createChargeOneStep(Gerencianet gn, Map<String, Object> card) async {
+  dynamic paymentToken = await gn.call("paymentToken", body: card);
+
   dynamic body = {
   "items": [
     {"name": "Product 1", "value": 1000, "amount": 2}
