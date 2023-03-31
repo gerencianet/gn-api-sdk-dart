2948541d63f4ef96aebf9f0f333e238dfa0c87ce
Guilherme Cota <75252715+guilhermecotaGn@users.noreply.github.com>
2023-03-31 10:56:15 -0300

    Update oneStepCard.dart

--- example/default/charge/oneStepCard.dart
+++ example/default/charge/oneStepCard.dart
@@ -11,25 +11,34 @@ void main() async {
 
 dynamic createChargeOneStep(Gerencianet gn) async {
   dynamic body = {
-    "items": [
-      {"name": "Product 1", "value": 1000, "amount": 2}
-    ],
-    "shippings": [
-      {"name": "Default Shipping Cost", "value": 100}
-    ],
-    "payment": {
-      "banking_billet": {
-        "expire_at": "2021-12-12",
-        "customer": {
-          "name": "Gorbadoc Oldbuck",
-          "email": "oldbuck@gerencianet.com.br",
-          "cpf": "94271564656",
-          "birth": "1977-01-15",
-          "phone_number": "5144916523"
-        }
+  "items": [
+    {"name": "Product 1", "value": 1000, "amount": 2}
+  ],
+  "shippings": [
+    {"name": "Default Shipping Cost", "value": 100}
+  ],
+  "payment": {
+    "credit_card": {
+      "installments": 1,
+      "payment_token": paymentToken['data']['payment_token'],
+      "billing_address": {
+        "street": "Av. JK",
+        "number": 909,
+        "neighborhood": "Bauxita",
+        "zipcode": "35400000",
+        "city": "Ouro Preto",
+        "state": "MG"
+      },
+      "customer": {
+        "name": "Gorbadoc Oldbuck",
+        "email": "oldbuck@gerencianet.com.br",
+        "cpf": "94271564656",
+        "birth": "1977-01-15",
+        "phone_number": "5144916523"
       }
     }
-  };
+  }
+};
 
   return await gn.call("oneStep", body: body);
 }
